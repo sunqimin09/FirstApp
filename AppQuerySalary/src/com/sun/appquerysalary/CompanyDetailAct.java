dad0e893e95304f11296c306b555f78cb7e67175
sunqmwork <sunqimin09@163.com>
2013-12-22 16:05:42 +0800

    公司提交

---
+++ AppQuerySalary/src/com/sun/appquerysalary/CompanyDetailAct.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sun.appquerysalary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 公司详细，包括公司的职位列表
+ */
+public class CompanyDetailAct extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.company_detail_act);
+	}
+
+}
---
+++ AppQuerySalary/src/com/sun/appquerysalary/JobSalaryAct.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sun.appquerysalary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ */
+public class JobSalaryAct extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
