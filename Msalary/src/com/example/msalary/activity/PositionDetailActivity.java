c713ca15c9b8b0dfb1e568dedd62af36316d5774
sqm <sunqimin09@163.com>
2013-12-30 07:35:03 +0800

    详情。。。解析

--- Msalary/src/com/example/msalary/activity/PositionDetailActivity.java
+++ Msalary/src/com/example/msalary/activity/PositionDetailActivity.java
@@ -9,9 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.msalary.R;
+import com.example.msalary.entity.JobEntity;
 import com.example.msalary.entity.RequestEntity;
 import com.example.msalary.entity.ResponseResult;
+import com.example.msalary.entity.ShowResult;
 import com.example.msalary.internet.InternetHelper;
+import com.example.msalary.json.JsonPositionDetail;
 import com.example.msalary.util.MConstant;
 
 /**
@@ -52,6 +55,36 @@ public class PositionDetailActivity extends BaseActivity{
 	@Override
 	public void requestSuccess(ResponseResult responseResult) {
 		super.requestSuccess(responseResult);
+		ShowResult showResult= JsonPositionDetail.parse(responseResult, this);
+		
+	}
+	
+	private void doReusult(ShowResult showResult){
+		int count = showResult.list.size();
+		int[] d = new int[6];
+		int salary = 0;
+		for(int i =0;i<count;i++){
+			salary = ((JobEntity)showResult.list.get(i)).getSalary();
+			if(salary<3000){
+				d[0] ++;
+			}else if(salary<6000){
+				d[1] ++;
+			}else if(salary<9000){
+				d[2] ++;
+			}else if(salary<15000){
+				d[3] ++;
+			}else if(salary<30000){
+				d[4] ++;
+			}else{
+				d[5] ++;
+			}
+		}
+		int sum = 0;
+		for(int j = 0;j<5;j++){
+			d[j] =(int) (d[j]/count*100);
+			sum =sum+d[j];
+		}
+		d[5] = count -sum;
 		
 	}
 	
