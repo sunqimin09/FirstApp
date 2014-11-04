cf408e2b0220d8a8f817d23cdb82a2450786660c
HelloWorld <sunqimin09@163.com>
2014-11-04 09:02:02 +0800

    图片

---
+++ AppDianPingHair02/src/com/sun/hair/act/AddPicAct.java
@@ -0,0 +1,23 @@
+package com.sun.hair.act;
+
+import com.sun.hair.BaseAct;
+/**
+ * ÃÌº”Õº∆¨
+ * @author sunqm
+ *
+ */
+public class AddPicAct extends BaseAct{
+
+	@Override
+	public void initTitle() {
+		
+		
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
